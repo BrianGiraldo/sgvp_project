170d528e04c4d553c40f19128cdb2894e06b5982
Brian <brian-giraldom@unilibre.edu.co>
2019-09-03 14:03:41 -0500

    Improvements

--- src/app/services/permissions.guard.ts
+++ src/app/services/permissions.guard.ts
@@ -15,21 +15,21 @@ export class PermissionsGuard implements CanActivate {
     this.userInfo = JSON.parse(localStorage.getItem('user'));
     const ups = this.userInfo.type;
 
-    if (state.url === '/usuarios') {
-      if (ups === 3) {
+    if (state.url == '/usuarios') {
+      if (ups == 3) {
         return true;
       } else {
         this.router.navigate(['/inicio']);
         return false;
       }
-    } else if (state.url === '/ofertas') {
-      if (ups === 1 || ups === 3 || ups === 2) {
+    } else if (state.url == '/ofertas') {
+      if (ups == 1 || ups == 3 || ups == 2) {
         return true;
       } else {
         this.router.navigate(['/inicio']);
         return false;
       }
     }
-    return state.url === '/inicio';
+    return state.url == '/inicio';
   }
 }
